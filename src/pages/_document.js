742acd0aa47a1f7749ac17e1e1fde393c188790a
Misael-GC <misael.gomez1623@gmail.com>
2022-10-11 00:59:24 -0500

    resolviendo las recomendaciones de lint:fix, re-configurando _document y _app con la sintaxis correcta y reubicando elementos, ignorando reglas muy estrictas en prettier

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,21 +1,11 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import Script from 'next/script';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* <title>React Shop</title> */}
-          <Script src="https://www.googletagmanager.com/gtag/js?id=G-DKQGKHGZ5Q" strategy="afterInteractive" />
-          <script
-            dangerouslySetInnerHTML={{
-              __html: `window.dataLayer = window.dataLayer || [];
-                              function gtag(){dataLayer.push(arguments);}
-                              gtag('js', new Date());
-                              gtag('config', 'G-DKQGKHGZ5Q');`,
-            }}
-          />
+          <meta charSet="utf-8" />
         </Head>
         <body>
           <Main />
